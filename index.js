2251793d779ea30f52037670c494cbc39ad08ffd
Karma-Chuki <109060808+Karma-Chuki@users.noreply.github.com>
2024-06-20 10:59:51 +0530

    comments

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+//javascript is for the functionality
 const bodyEl =document.querySelector("body")
 
 bodyEl.addEventListener("mousemove", (event)=>{
     const xPos = event.offsetX;
     const yPos = event.offsetY;
     const spanEl = document.createElement("span");
-    spanEl.style.left = xPos + "px";
+    spanEl.style.left = xPos + "px";   //px is for pixel
     spanEl.style.top = yPos + "px";
     const size = Math.random()*100;
     spanEl.style.width = size + "px";
@@ -12,7 +13,7 @@ bodyEl.addEventListener("mousemove", (event)=>{
     bodyEl.appendChild(spanEl);
     setTimeout(()=>{
         spanEl.remove();
-    }, 3000);
+    }, 3000);  //3 seconds
 })
 
 //i made some changes
